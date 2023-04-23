9e1cc288780c77543790162ad8238bcb0eb666dc
Pande <popovski.pande@gmail.com>
2023-04-24 00:44:27 +0200

    Complete Header
    Finished coding the header content. Added buttons for visiting my
    github profile and toggling sound, which are both functional.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import clickSound from "./assets/clickSound.mp3";
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [sound, setSound] = useState(false);
+
+  const toggleSound = () => {
+    sound === false ?
+      setSound(true) :
+      setSound(false);
+  }
+
+  const playSound = () => {
+    const audio = new Audio(clickSound);
+    if (sound === true) audio.play();
+  }
+
   return (
     <div className="App">
-      <Header />
+      <Header 
+        score={score} sound={sound} toggleSound={toggleSound}
+        playSound={playSound} />
       <Main />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,63 @@
 import React from "react";
+import { Button, Container, Icon, Typography } from '@mui/material';
+import PsychologyAltOutlinedIcon from '@mui/icons-material/PsychologyAltOutlined';
+import VolumeUpOutlinedIcon from '@mui/icons-material/VolumeUpOutlined';
+import VolumeOffOutlinedIcon from '@mui/icons-material/VolumeOffOutlined';
+
 
 export default function Header(props) {
+  const handleAudioClick = () => {
+    props.toggleSound();
+    props.playSound();
+  }
+
   return(
     <div className="header">
-      Header
+      <div className="header-left">
+          <Typography 
+            variant="h3" color="white" fontStyle="italic" fontWeight="700">
+              Memory Card Game
+          </Typography>
+          <Button 
+            variant="contained" color="primary" 
+            href="https://github.com/Duesenberg" target="_blank"
+            onClick={props.playSound}>
+              <PsychologyAltOutlinedIcon fontSize="large" />
+          </Button>
+      </div>
+
+      <div className="header-right">
+        <div className="sound">
+          <Typography variant="h5" color="white">Sound:</Typography>
+          {props.sound === false ?
+            <Button 
+              variant="contained" 
+              color="primary" 
+              onClick={props.toggleSound}>
+                <VolumeOffOutlinedIcon fontSize="large" />
+            </Button> :
+            <Button 
+              variant="contained" 
+              color="primary" 
+              onClick={props.toggleSound}>
+                <VolumeUpOutlinedIcon fontSize="large" />
+            </Button>
+          }
+        </div>
+        <div className="score">
+          <Typography 
+            variant="h5" 
+            color="white">
+              Score:
+          </Typography>
+          <Typography 
+            variant="h6" 
+            color="white" 
+            textAlign="center">
+              {props.score}
+          </Typography>
+        </div>
+      </div>
     </div>
   )
 }
